dab980c574897d4e305ec4773803d76058600ab3
Kevin Ndung'u <kevgathuku@gmail.com>
2018-10-03 18:49:28 +0300

    Add instructions section

--- src/App.js
+++ src/App.js
@@ -7,43 +7,49 @@ class App extends Component {
   constructor(...props) {
     super(...props);
     this.state = {
-      stageWidth: 1200,
-      stageHeight: 800,
+      stageWidth: 800,
+      stageHeight: 700,
       color: "#000000",
-      startX: 400,
-      startY: 50,
-      width: 700,
-      height: 700
+      startX: 0,
+      startY: 0,
+      width: 800,
+      height: 700,
+      mode: null,
     };
   }
 
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
+        <div className="intro content">
+          <h2>Pong</h2>
+          <h3>Single Player Mode</h3>
+          <p>Use keys A and Z to move the left paddle up and down</p>
+          <button className="button is-primary">
+            Start Single Player Mode
+          </button>
+          <h3>Two Player Mode</h3>
+          <p>
+            Player 1: Move the left paddle up and down using keys A and Z
+          </p>
+          <p>
+            Player 2: Move the right paddle up and down using the Up and
+            Down Arrows
+          </p>
+          <button className="button is-primary">
+            Start Two Player Mode
+          </button>
+          <p/>
+          <p>The first player to reach a score of 10 wins the game</p>
+          <h3>GOOD LUCK!</h3>
+        </div>
         <Stage width={this.state.stageWidth} height={this.state.stageHeight}>
           <Layer>
-            <Rect
-              x={this.state.startX}
-              y={this.state.startY}
-              width={this.state.width}
-              height={this.state.height}
-              fill={this.state.color}
-              shadowBlur={10}
-            />
-            <Paddle
-              position="left"
-              startX={this.state.startX}
-              startY={this.state.startY}
-            />
-            <Paddle
-              position="right"
-              startX={this.state.startX + this.state.width}
-              startY={this.state.startY}
-            />
+            <Rect x={this.state.startX} y={this.state.startY} width={this.state.width} height={this.state.height} fill={this.state.color} shadowBlur={10} />
+            <Paddle position="left" startX={this.state.startX + 20} startY={this.state.startY + 50} />
+            <Paddle position="right" startX={this.state.startX + this.state.width - 20 - 20} startY={this.state.startY + 50} />
           </Layer>
         </Stage>
-      </div>
-    );
+      </div>;
   }
 }
 
--- src/Paddle.jsx
+++ src/Paddle.jsx
@@ -1,29 +1,43 @@
 import React, { Component } from "react";
 import { Rect } from "react-konva";
+import PropTypes from "prop-types";
 
 class Paddle extends Component {
+  static propTypes = {
+    position: PropTypes.string.isRequired
+  };
+
   constructor(...props) {
     super(...props);
     this.state = {
       color: "white",
       width: 20,
-      height: 100,
-      offsetY: 50
+      height: 100
     };
   }
 
+  onKeyPress = event => {
+    const { position } = this.props;
+    console.log("event", event);
+
+    // if (position === "left") {}
+  };
+
   render() {
     const { startX, startY, position } = this.props;
-    let offsetX = position === "left" ? 20 : (-20 - this.state.width);
+    // let offsetX = position === "left" ? 20 : undefined;
+    // offsetX = position === "right" ? (-20 - this.state.width) : undefined;
+    if (!position) return null;
 
     return (
       <Rect
-        x={startX + offsetX}
-        y={startY + this.state.offsetY}
+        x={startX}
+        y={startY}
         width={this.state.width}
         height={this.state.height}
         fill={this.state.color}
         shadowBlur={10}
+        onKeyPress={this.onKeyPress}
       />
     );
   }
