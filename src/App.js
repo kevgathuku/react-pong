2b37f49b21b630176211b5db24d2ed95d72e46a4
Kevin Ndung'u <kevgathuku@gmail.com>
2018-10-25 10:34:48 +0300

    Update instructions

--- src/App.js
+++ src/App.js
@@ -1,101 +1,96 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Layer, Rect, Stage } from "react-konva";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Layer, Rect, Stage } from 'react-konva';
+import { connect } from 'react-redux';
 
-import { keyPress, keyUp } from "./store/actions";
+import { keyPress, keyUp } from './store/actions';
 
-import "./App.css";
-import Paddle from "./Paddle";
+import './App.css';
+import Paddle from './components/Paddle';
 
 class PongApp extends Component {
-  static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    players: PropTypes.array,
-    gameWidth: PropTypes.number.isRequired,
-    gameHeight: PropTypes.number.isRequired,
-    boardColor: PropTypes.string.isRequired
-  };
+	static propTypes = {
+		dispatch: PropTypes.func.isRequired,
+		players: PropTypes.array.isRequired,
+		gameWidth: PropTypes.number.isRequired,
+		gameHeight: PropTypes.number.isRequired,
+		boardColor: PropTypes.string.isRequired,
+	};
 
-  componentDidMount() {
-    window.addEventListener("keydown", this.onKeyDown);
-    window.addEventListener("keyup", this.onKeyUp);
-  }
+	componentDidMount() {
+		window.addEventListener('keydown', this.onKeyDown);
+		window.addEventListener('keyup', this.onKeyUp);
+	}
 
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.onKeyDown);
-    window.removeEventListener("keyup", this.onKeyUp);
-  }
+	componentWillUnmount() {
+		window.removeEventListener('keydown', this.onKeyDown);
+		window.removeEventListener('keyup', this.onKeyUp);
+	}
 
-  onKeyDown = event => {
-    const { dispatch } = this.props;
+	onKeyDown = event => {
+		const { dispatch } = this.props;
 
-    switch (event.keyCode) {
-      case 38: // Down Arrow
-      case 40: // Up Arrow
-      case 65: // a
-      case 90: // z
-        console.log("Key", event);
-        dispatch(keyPress(event.key));
-        break;
-      default:
-        return; // Do nothing
-    }
-  };
+		switch (event.keyCode) {
+			case 87: // W
+			case 83: // S
+				console.log('Key', event);
+				dispatch(keyPress(event.key));
+				break;
+			default:
+				return; // Do nothing
+		}
+	};
 
-  onKeyUp = event => {
-    const { dispatch } = this.props;
+	onKeyUp = event => {
+		const { dispatch } = this.props;
 
-    dispatch(keyUp(event.key));
-  };
+		dispatch(keyUp(event.key));
+	};
 
-  render() {
-    const { boardColor, players, gameWidth, gameHeight } = this.props;
-    return (
-      <div className="App">
-        <div className="intro content">
-          <h2>Pong</h2>
-          <h3>Single Player Mode</h3>
-          <p>Use keys A and Z to move the left paddle up and down</p>
-          <button className="button is-primary">
-            Start Single Player Mode
-          </button>
-          <h3>Two Player Mode</h3>
-          <p>Player 1: Move the left paddle up and down using keys A and Z</p>
-          <p>
-            Player 2: Move the right paddle up and down using the Up and Down
-            Arrows
-          </p>
-          <button className="button is-primary">Start Two Player Mode</button>
-          <p />
-          <p>The first player to reach a score of 10 wins the game</p>
-          <h3>GOOD LUCK!</h3>
-        </div>
-        <Stage width={gameWidth} height={gameHeight}>
-          <Layer>
-            <Rect
-              x={0}
-              y={0}
-              width={gameWidth}
-              height={gameHeight}
-              fill={boardColor}
-              shadowBlur={10}
-            />
+	render() {
+		const {
+			boardColor,
+      players,
+			gameWidth,
+			gameHeight,
+		} = this.props;
+		return (
+			<div className="App">
+				<div className="intro content">
+					<h2>Pong</h2>
+					<h3>Single Player Mode</h3>
+					<p>Use keys W and S to move the left paddle up and down respectively</p>
+					<button className="button is-primary">
+						Start Single Player Mode
+					</button>
+					<p />
+					<p>The first player to reach a score of 5 wins the game</p>
+					<h3>GOOD LUCK!</h3>
+				</div>
+				<Stage width={gameWidth} height={gameHeight}>
+					<Layer>
+						<Rect
+							x={0}
+							y={0}
+							width={gameWidth}
+							height={gameHeight}
+							fill={boardColor}
+							shadowBlur={10}
+						/>
             {players.map(player => (
               <Paddle player={player} key={player.position} />
             ))}
-          </Layer>
-        </Stage>
-      </div>
-    );
-  }
+
+					</Layer>
+				</Stage>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  const { players, gameWidth, gameHeight, boardColor, keysPressed } = state;
-  return { players, gameWidth, gameHeight, boardColor, keysPressed };
+	const { players, gameWidth, gameHeight, boardColor, keysPressed } = state;
+	return { players, gameWidth, gameHeight, boardColor, keysPressed };
 };
 
-const VisiblePongApp = connect(mapStateToProps)(PongApp);
-
-export default VisiblePongApp;
+export default connect(mapStateToProps)(PongApp);
