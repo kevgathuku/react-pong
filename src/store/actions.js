7b034dd1cb7003b372704f8f5aa008de208e6a9b
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-04 18:55:28 +0300

    Add move ball functionality

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,27 +1,37 @@
 import { ActionTypes } from './types';
 
 export const keyPress = (key = null) => ({
-	type: ActionTypes.KEYPRESS,
-	payload: key,
+  type: ActionTypes.KEYPRESS,
+  payload: key,
 });
 
 export const keyUp = (key = null) => ({
-	type: ActionTypes.KEY_UP,
-	payload: key,
+  type: ActionTypes.KEY_UP,
+  payload: key,
 });
 
 export const movePaddleDown = (position, velocity) => ({
-	type: ActionTypes.MOVE_PADDLE_DOWN,
-	payload: { position },
+  type: ActionTypes.MOVE_PADDLE_DOWN,
+  payload: { position },
 });
 
 export const movePaddleUp = (position, velocity) => ({
-	type: ActionTypes.MOVE_PADDLE_UP,
-	payload: { position },
+  type: ActionTypes.MOVE_PADDLE_UP,
+  payload: { position },
 });
 
 export const serveBall = () => ({
-	type: ActionTypes.SERVE_BALL_RANDOM,
+  type: ActionTypes.SERVE_BALL_RANDOM,
 });
 
 export const moveBall = () => ({ type: ActionTypes.MOVE_BALL });
+
+export const setBallPosition = (position) => ({
+  type: ActionTypes.SET_BALL_POSITION,
+  payload: {
+		x: position.x,
+		y: position.y,
+		x_speed: position.x_speed,
+		y_speed: position.y_speed
+	},
+});
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -11,112 +11,129 @@ const PADDLE_OFFSET_Y = 50;
 const PADDLE_SPEED = 20;
 
 const randomDirection = () => {
-	let direction = Math.random();
-	if (direction > 0.5) {
-		return -1 * direction;
-	} else {
-		return direction;
-	}
+  let direction = Math.random();
+  if (direction > 0.5) {
+    return -1 * direction;
+  } else {
+    return direction;
+  }
 };
 
 const humanPaddle = {
-	controller: 'human',
-	position: 'left',
-	x: PADDLE_OFFSET_X,
-	y: PADDLE_OFFSET_Y,
-	width: PADDLE_WIDTH,
-	height: PADDLE_HEIGHT,
+  controller: 'human',
+  position: 'left',
+  x: PADDLE_OFFSET_X,
+  y: PADDLE_OFFSET_Y,
+  width: PADDLE_WIDTH,
+  height: PADDLE_HEIGHT,
 };
 
 const computerPaddle = {
-	controller: 'computer',
-	position: 'right',
-	x: GAME_WIDTH - PADDLE_OFFSET_X - PADDLE_WIDTH,
-	y: PADDLE_OFFSET_Y,
-	width: PADDLE_WIDTH,
-	height: PADDLE_HEIGHT,
+  controller: 'computer',
+  position: 'right',
+  x: GAME_WIDTH - PADDLE_OFFSET_X - PADDLE_WIDTH,
+  y: PADDLE_OFFSET_Y,
+  width: PADDLE_WIDTH,
+  height: PADDLE_HEIGHT,
 };
 
 const ball = {
-	x: GAME_WIDTH / 2,
-	y: GAME_HEIGHT / 2,
-	radius: 10,
-	x_speed: 0,
-	y_speed: 0,
+  x: GAME_WIDTH / 2,
+  y: GAME_HEIGHT / 2,
+  radius: 10,
+  x_speed: 5,
+  y_speed: 5,
 };
 
 const initialState = {
-	boardColor: '#000000',
-	mode: 'paused',
-	gameWidth: GAME_WIDTH,
-	gameHeight: GAME_HEIGHT,
-	velocity: PADDLE_SPEED,
-	players: [humanPaddle, computerPaddle],
-	ball,
-	keysPressed: {},
+  boardColor: '#000000',
+  mode: 'paused',
+  gameWidth: GAME_WIDTH,
+  gameHeight: GAME_HEIGHT,
+  velocity: PADDLE_SPEED,
+  players: [humanPaddle, computerPaddle],
+  ball,
+  keysPressed: {},
 };
 
 const reducer = (state = initialState, action) => {
-	const { velocity } = state;
-	const { type, payload } = action;
-	switch (type) {
-		case ActionTypes.START_GAME:
-			return Object.assign({}, state, { mode: 'playing' });
-		case ActionTypes.KEYPRESS:
-			return Object.assign({}, state, {
-				keysPressed: {
-					...state.keysPressed,
-					[payload]: true,
-				},
-			});
-		case ActionTypes.KEY_UP:
-			return Object.assign({}, state, {
-				keysPressed: {
-					...state.keysPressed,
-					[payload]: false,
-				},
-			});
-		case ActionTypes.MOVE_PADDLE_UP:
-			const { position } = payload;
-			const updatedPlayers = state.players.map(player => {
-				if (player.position === position) {
-					// Decrease the Y value
-					player.y -= velocity;
-					// Top of the board
-					if (player.y < 0) {
-						player.y = 0;
-					}
-				}
-				return player;
-			});
+  const { velocity } = state;
+  const { type, payload } = action;
+  switch (type) {
+    case ActionTypes.START_GAME:
+      return Object.assign({}, state, { mode: 'playing' });
+    case ActionTypes.KEYPRESS:
+      return Object.assign({}, state, {
+        keysPressed: {
+          ...state.keysPressed,
+          [payload]: true,
+        },
+      });
+    case ActionTypes.KEY_UP:
+      return Object.assign({}, state, {
+        keysPressed: {
+          ...state.keysPressed,
+          [payload]: false,
+        },
+      });
+    case ActionTypes.MOVE_PADDLE_UP:
+      const { position } = payload;
+      const updatedPlayers = state.players.map(player => {
+        if (player.position === position) {
+          // Decrease the Y value
+          player.y -= velocity;
+          // Top of the board
+          if (player.y < 0) {
+            player.y = 0;
+          }
+        }
+        return player;
+      });
 
-			return Object.assign({}, state, { players: updatedPlayers });
-		case ActionTypes.MOVE_PADDLE_DOWN:
-			const updatedPaddles = state.players.map(player => {
-				if (player.position === payload.position) {
-					player.y += velocity;
-					// Bottom of the board
-					if (player.y + PADDLE_HEIGHT > GAME_HEIGHT) {
-						player.y = GAME_HEIGHT - PADDLE_HEIGHT;
-					}
-				}
-				return player;
-			});
-			return Object.assign({}, state, { players: updatedPaddles });
-		case ActionTypes.SERVE_BALL_RANDOM:
-			// Set the ball's direction and speed
-			return produce(state, draftState => {
-				draftState.ball.x_speed = 5 * randomDirection();
-				draftState.ball.y_speed = 5 * randomDirection();
-			});
-		case ActionTypes.MOVE_BALL:
-			return produce(state, draftState => {
-				draftState.ball.x = draftState.ball.x + draftState.ball.x_speed;
-				draftState.ball.y = draftState.ball.y + draftState.ball.y_speed;
-			});
-		default:
-			return state;
-	}
+      return Object.assign({}, state, { players: updatedPlayers });
+    case ActionTypes.MOVE_PADDLE_DOWN:
+      const updatedPaddles = state.players.map(player => {
+        if (player.position === payload.position) {
+          player.y += velocity;
+          // Bottom of the board
+          if (player.y + PADDLE_HEIGHT > GAME_HEIGHT) {
+            player.y = GAME_HEIGHT - PADDLE_HEIGHT;
+          }
+        }
+        return player;
+      });
+      return Object.assign({}, state, { players: updatedPaddles });
+    case ActionTypes.SERVE_BALL_RANDOM:
+      // Set the ball's direction and speed
+      return produce(state, draftState => {
+        draftState.ball.x += draftState.ball.x_speed * randomDirection();
+        draftState.ball.y += draftState.ball.y_speed * randomDirection();
+      });
+    case ActionTypes.SET_BALL_POSITION:
+      break;
+    case ActionTypes.MOVE_BALL:
+      return produce(state, draftState => {
+        const top_y = draftState.ball.y - draftState.ball.radius;
+        const right_x = draftState.ball.x + draftState.ball.radius;
+        const bottom_y = draftState.ball.y + draftState.ball.radius;
+				const left_x = draftState.ball.x - draftState.ball.radius;
+
+				draftState.ball.x += draftState.ball.x_speed;
+        draftState.ball.y += draftState.ball.y_speed;
+
+        // Hitting the top boundary
+        if (draftState.ball.y - draftState.ball.radius < 0) {
+          draftState.ball.y = draftState.ball.radius; // Don't go beyond the boundary
+          draftState.ball.y_speed = -draftState.ball.y_speed + Math.random(); // Reverse the direction
+        } // Hitting the bottom boundary
+        else if (draftState.ball.y + draftState.ball.radius > draftState.gameHeight) {
+          draftState.ball.y = draftState.gameHeight - draftState.ball.radius; // Set the new position
+          draftState.ball.y_speed = -draftState.ball.y_speed - Math.random(); // Reverse direction
+        }
+      });
+    default:
+      return state;
+  }
 };
 
 export default reducer;
