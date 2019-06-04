02f8e6f0cd61cc7acc20ab6c36e2c186ad31f8fe
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-04 18:53:51 +0300

    Update Ball component to use Pixi

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
-import { Circle } from 'react-konva';
+import { Graphics } from '@inlet/react-pixi';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { moveBall, serveBall } from '../store/actions'
 
 class Ball extends Component {
-	static propTypes = {
-		dispatch: PropTypes.func.isRequired,
-		data: PropTypes.object.isRequired,
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    data: PropTypes.object.isRequired,
 	};
 
-	render() {
-		const {
-			data: { x, y, radius },
-		} = this.props;
+	componentDidMount() {
+    // listen to tick events (raf)
+    this.props.app.ticker.add(this.tick);
+  }
 
-		return <Circle x={x} y={y} fill="#00FFFF" radius={radius} />;
+  componentWillUnmount() {
+    // stop listening for tick events
+    this.props.app.ticker.remove(this.tick);
 	}
+
+	tick = delta => {
+		const { dispatch } = this.props;
+		dispatch(moveBall())
+  };
+
+  render() {
+    const {
+			dispatch,
+      data: { x, y, radius },
+    } = this.props;
+
+    return (
+      <Graphics
+        draw={g => {
+          // clear the graphics
+          g.clear();
+          // start drawing
+          g.beginFill(0xffffff, 0.5);
+          g.drawCircle(x, y, radius);
+          g.endFill();
+				}}
+				interactive
+				pointerdown={() => {
+					console.log('click')
+					dispatch(serveBall())
+				}}
+      />
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	const { ball } = state;
-	return { data: ball };
+  const { ball, gameWidth, gameHeight } = state;
+  return { data: ball, gameWidth, gameHeight };
 };
 
 export default connect(mapStateToProps)(Ball);
