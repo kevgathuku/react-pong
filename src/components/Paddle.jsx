49f12aec88423fe0c6086c9ed9b641dcb4bb0da3
Kevin Ndung'u <kevgathuku@gmail.com>
2020-03-26 08:26:48 +0300

    Refactor Paddle to functional component

--- src/components/Paddle.jsx
+++ src/components/Paddle.jsx
@@ -1,61 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Graphics } from '@inlet/react-pixi';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 
 import { movePaddleDown, movePaddleUp } from '../store/actions';
 
-class Paddle extends Component {
-  static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    player: PropTypes.object.isRequired,
-    keysPressed: PropTypes.object,
-  };
+function Paddle(props) {
+  const {
+    player: { x, y, position, width, height },
+    keysPressed,
+    dispatch,
+  } = props;
 
-  constructor(...props) {
-    super(...props);
-    this.state = {
-      color: 'white',
-    };
-  }
+  if (!position) return null;
 
-  render() {
-    const {
-      player: { x, y, position, width, height },
-      keysPressed,
-      dispatch,
-    } = this.props;
-    if (!position) return null;
-
-    if (position === 'left') {
-      if (keysPressed['a']) {
-        dispatch(movePaddleUp(position));
-      }
-      if (keysPressed['z']) {
-        dispatch(movePaddleDown(position));
-      }
-    } else if (position === 'right') {
-      if (keysPressed['ArrowUp']) {
-        dispatch(movePaddleUp(position));
-      }
-      if (keysPressed['ArrowDown']) {
-        dispatch(movePaddleDown(position));
-      }
+  // TODO: Figure out if dispatch should be called in some other hook
+  if (position === 'left') {
+    if (keysPressed['a']) {
+      dispatch(movePaddleUp(position));
     }
-
-    return (
-      <Graphics
-        draw={g => {
-          // clear the graphics
-          g.clear();
-          // start drawing
-          if (position === 'left') g.beginFill(0xf977a3);
-          if (position === 'right') g.beginFill(0x66ddd4);
-          g.drawRect(x, y, width, height);
-        }}
-      />
-    );
+    if (keysPressed['z']) {
+      dispatch(movePaddleDown(position));
+    }
+  } else if (position === 'right') {
+    if (keysPressed['ArrowUp']) {
+      dispatch(movePaddleUp(position));
+    }
+    if (keysPressed['ArrowDown']) {
+      dispatch(movePaddleDown(position));
+    }
   }
+
+  return (
+    <Graphics
+      draw={g => {
+        // clear the graphics
+        g.clear();
+        // start drawing
+        if (position === 'left') g.beginFill(0xf977a3);
+        if (position === 'right') g.beginFill(0x66ddd4);
+        g.drawRect(x, y, width, height);
+      }}
+    />
+  );
 }
 
 const mapStateToProps = state => {
