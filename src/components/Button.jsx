30b93af4f359a9e0899a11637370e64b9773b993
Kevin Ndung'u <kevgathuku@gmail.com>
2020-03-26 09:36:52 +0300

    Convert Button to functional component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,53 +1,50 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import * as PIXI from 'pixi.js';
-import PropTypes from 'prop-types';
 import { Text, Graphics, Container } from '@inlet/react-pixi';
+import PropTypes from 'prop-types';
 
-export default class Button extends Component {
-  static propTypes = {
-    action: PropTypes.func.isRequired,
-    data: PropTypes.object.isRequired,
-  };
+Button.propTypes = {
+  action: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired,
+};
 
-  render() {
-    const {
-      data: { x, y, top_x, top_y, text },
-    } = this.props;
+export default function Button(props) {
+  const {
+    data: { x, y, top_x, top_y, text },
+  } = props;
 
-    return (
-      <Container
-        interactive
-        pointerdown={() => {
-          this.props.action();
+  return (
+    <Container
+      interactive
+      pointerdown={() => {
+        props.action();
+      }}
+    >
+      <Text
+        text={text}
+        anchor={0.5}
+        x={x}
+        y={y}
+        isSprite
+        style={
+          new PIXI.TextStyle({
+            align: 'center',
+            fontFamily: 'Arial, sans-serif',
+            fontSize: 35,
+            fill: '#26f7a3',
+            // stroke: '#ffffff',
+            // strokeThickness: 2,
+            // letterSpacing: 10,
+          })
+        }
+      />
+      <Graphics
+        preventRedraw
+        draw={g => {
+          g.lineStyle(2, 0xfffff, 1);
+          g.drawRect(top_x, top_y, 250, 100);
         }}
-      >
-        <Text
-          text={text}
-          anchor={0.5}
-          x={x}
-          y={y}
-          isSprite
-          style={
-            new PIXI.TextStyle({
-              align: 'center',
-              fontFamily: 'Arial, sans-serif',
-              fontSize: 35,
-              fill: '#26f7a3',
-              // stroke: '#ffffff',
-              // strokeThickness: 2,
-              // letterSpacing: 10,
-            })
-          }
-        />
-        <Graphics
-          preventRedraw
-          draw={g => {
-            g.lineStyle(2, 0xfffff, 1);
-            g.drawRect(top_x, top_y, 250, 100);
-          }}
-        />
-      </Container>
-    );
-  }
+      />
+    </Container>
+  );
 }
