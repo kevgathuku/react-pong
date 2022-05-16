5dd383ecf7301d8d98e342c8a6d57e0cabf6e40a
Kevin Gathuku <kevgathuku@gmail.com>
2022-05-16 22:00:22 +0300

    Add Counter example powered by redux toolkit

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import "../src/App.css";
+
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import store from "../src/app/store";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+}
