bb6e5036a25802236b6085d5fab50e8542bd03be
Kevin Gathuku <kevgathuku@gmail.com>
2022-05-17 12:31:28 +0300

    Remove storybook

--- .storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.js'],
-  addons: ['@storybook/preset-create-react-app', '@storybook/addon-actions', '@storybook/addon-links'],
-  core: {
-    builder: 'webpack5'
-  }
-};
