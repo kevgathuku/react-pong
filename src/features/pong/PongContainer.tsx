36418a706ed533c1fb64e591cf8e4e74fb5bfece
Kevin Gathuku <kevgathuku@gmail.com>
2023-09-17 22:57:56 +0200

    Convert from Pixi components to SVG

--- src/features/pong/PongContainer.tsx
+++ src/features/pong/PongContainer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { Stage } from "@inlet/react-pixi";
 import { useDispatch, useSelector } from "react-redux";
 
 import {
@@ -35,13 +34,58 @@ export const PongApp = (props) => {
 
   return (
     <div className="appContainer">
-      <Stage
+      {/* <Stage
         width={config.width}
         height={config.height}
         options={{ autoDensity: true, backgroundColor: config.boardColor }}
       >
-        <Pong {...pongContainerProps} />
-      </Stage>
+      <Pong {...pongContainerProps} />        
+      </Stage> */}
+      <svg
+        viewBox={`0 0 ${config.width} ${config.height}`}
+        width={`${config.width}`}
+        height={`${config.height}`}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {/* Background */}
+        <rect
+          x="0"
+          y="0"
+          width={`${config.width}`}
+          height={`${config.height}`}
+        />
+        <text
+          x={`${config.width / 2 - 100}`}
+          y="50"
+          fill="white"
+          style={{
+            fontFamily: "Futura, sans-serif",
+            fontSize: "40px",
+            letterSpacing: "10",
+          }}
+        >
+          PONG!
+        </text>
+        <g>
+          <rect
+            x={`${config.width / 2 - 150}`}
+            y={`${config.height / 2 - 100}`}
+            width="250"
+            height="100"
+            stroke="#0fffff"
+            strokeWidth={2}
+          />
+          <text
+            x="325"
+            y="310"
+            font-family="Arial, sans-serif"
+            font-size="35"
+            fill="#26f7a3"
+          >
+            START
+          </text>
+        </g>
+      </svg>
     </div>
   );
 };
